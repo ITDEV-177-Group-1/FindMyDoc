3064b1078fc596ed691588c98758cb48150313a6
GioHernan <florem88@gmatc.matc.edu>
2025-05-06 21:38:12 -0500

    CSS fixed

--- js/script.js
+++ js/script.js
@@ -4,19 +4,19 @@
         const modal = document.getElementById('user-form-modal');
         const closeButton = document.getElementById('close-modal-button');
 
-        // Show modal on Enter key press
+        
         searchInput.addEventListener('keydown', function (e) {
             if (e.key === 'Enter') {
-                e.preventDefault(); // Prevent form submission
+                e.preventDefault(); 
                 if (searchInput.value.trim() !== '') {
                     modal.style.display = 'flex';
                 }
             }
         });
 
-        // Close modal
+        
         closeButton.addEventListener('click', function (e) {
-            e.preventDefault(); // Prevent form submission
+            e.preventDefault(); 
             modal.style.display = 'none';
         });
     });
